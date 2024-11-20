6427aaabd86109b827e268d4c0e0378354d8a82f
Audrius Molis <vienas00@gmail.com>
2024-11-20 19:54:43 +0200

    Add empty module scaffold

---
+++ audrius_m_mod1_atsiskaitymas/crawler.py
@@ -0,0 +1,2 @@
+def crawl(time_limit=60, source='lrytas', return_format='csv'):
+    print(f"Todo crawling now with parameters time_limit={time_limit}, source={source}, return_format={return_format}")
