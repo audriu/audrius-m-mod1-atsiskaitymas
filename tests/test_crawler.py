6c53982a66a170239aa78f9868d99bf910235ab9
Audrius Molis <vienas00@gmail.com>
2024-11-25 21:24:07 +0200

    Fix tests import

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,4 +1,7 @@
 import unittest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from audrius_m_mod1_atsiskaitymas.crawler import crawl
 
 
